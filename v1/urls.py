08e60116eaf38cca4f5ec0709fad3b946205d9db
mahyargorji <mhrgrj@gmail.com>
2019-11-22 16:06:47 -0500

    finished urls and views

--- v1/urls.py
+++ v1/urls.py
@@ -1,13 +1,27 @@
 from django.urls import path
 from v1 import views
+from v1.models import Comments, Courses, Section_students, Sections, Team_Members, Teams, Threads, Users
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    # path('emails/', views.email_list.as_view()),
-    # path('emails/<int:pk>/', views.email_detail.as_view()),
-    # path('users/', views.user_list.as_view()),
-    # path('users/<int:pk>/', views.user_detail.as_view()),
+    path('comments/', views.Comments_List.as_view()),
+    path('comments/<int:pk>/', views.Comments_detail.as_view()),
+    path('courses/', views.Courses_list.as_view()),
+    path('courses/<int:pk>/', views.Courses_detail.as_view()),
+    path('section_students/', views.Section_students_list.as_view()),
+    path('section_students/<int:pk>/', views.Section_students_detail.as_view()),
+    path('sections/', views.Sections_list.as_view()),
+    path('sections/<int:pk>/', views.Sections_list.as_view()),
+    path('team_members/', views.Team_Members_list.as_view()),
+    path('team_members/<int:pk>/', views.Team_Members_detail.as_view()),
+    path('teams/', views.Teams_list.as_view()),
+    path('teams/<int:pk>/', views.Teams_detail().as_view()),
+    path('threads/', views.Threads_list.as_view()),
+    path('threads/<int:pk>/', views.Threads_detail.as_view()),
+    path('users/', views.Users_list.as_view()),
+    path('users/<int:pk>/', views.Users_detail.as_view()),
+
     # path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- v1/views.py
+++ v1/views.py
@@ -1,3 +1,110 @@
 from django.shortcuts import render
 
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+from v1.models import Email, User
+from v1.serializers import CommentsSerializer, CoursesSerializer, Section_studentsSerializer,
+from v1.serializers import SectionsSerializer, Team_MembersSerializer, TeamsSerializer, ThreadsSerializer, UsersSerializer
+#from v1.permissions import IsOwnerOrReadOnly <-- not implemented
+
+from rest_framework import status, permissions, generics, viewsets
+from rest_framework.response import Response
+
 # Create your views here.
+class Comments_List(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+#RetrieveUpdateDestroyAPIView
+#RetrieveAPIView sets to readonly
+class Comments_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+class Users_list(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+class Users_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Users.objects.all()
+    serializer_class = UsersSerializer
+
+class Courses_list(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Courses.objects.all()
+    serializer_class = CoursesSerializer
+
+class Courses_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Courses.objects.all()
+    serializer_class = CoursesSerializer
+
+class Section_students_list(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Section_students.objects.all()
+    serializer_class = Section_studentsSerializer
+
+class Section_students_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Section_students.objects.all()
+    serializer_class = Section_studentsSerializer
+
+class Sections_list(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Sections.objects.all()
+    serializer_class = SectionsSerializer
+
+class Sections_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Sections.objects.all()
+    serializer_class = SectionsSerializer
+
+class Team_Members_list(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Team_Members.objects.all()
+    serializer_class = SectionsSerializer
+
+class Team_Members_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Team_Members.objects.all()
+    serializer_class = Team_MembersSerializer
+
+class Teams_list(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Teams.objects.all()
+    serializer_class = TeamsSerializer
+
+class Teams_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Teams.objects.all()
+    serializer_class = TeamsSerializer
+
+class Threads_list(generics.ListCreateAPIView):
+    #permission_classes = [permissions.IsAuthenticated]
+
+    queryset = Threads.objects.all()
+    serializer_class = ThreadsSerializer
+
+class Threads_detail(generics.RetrieveUpdateDestroyAPIView):
+    #permission_classes = [permissions.IsAdminUser]
+
+    queryset = Threads.objects.all()
+    serializer_class = ThreadsSerializer
