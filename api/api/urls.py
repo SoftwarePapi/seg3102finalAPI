96d827d2c9da26f5889cb5305689b0d61c3fab7b
mahyargorji <mhrgrj@gmail.com>
2019-11-22 12:14:43 -0500

    changed master-level URLS.py

--- api/api/urls.py
+++ api/api/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.conf.urls import url, include
 from django.urls import path
+from django.contrib import admin
+from rest_framework import routers, serializers, viewsets
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'v1/', include('v1.urls')),
+    url(r'api-auth/', include('rest_framework.urls')),
 ]
